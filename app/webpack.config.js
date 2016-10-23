eb174a27972e7864f5ffcd83dfd0011434f983ec
Andrés Londoño <andres.londono@etecture.de>
2016-10-23 16:34:02 +0200

    23 OKT 2016 - 16:30

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,11 +1,18 @@
-var webpack = require('webpack');
-var path = require('path');
-var htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const validator = require('webpack-validator');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+const PATHS = {
+  app: path.join(__dirname, 'client/client.js'),
+  build: path.join(__dirname, 'build')
+}
+
+const common = {
   entry: [
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000',
-    './client/client.js'
+    PATHS.app
   ],
   output: {
     path: '/',
@@ -43,3 +50,15 @@ module.exports = {
     })
   ]
 }
+
+var config;
+
+switch (process.env.npm_lifecycle_event) {
+  case 'build':
+    conf = merge(common, {});
+    break;
+  default:
+    conf = merge(common, {});
+}
+
+module.exports = validator(config);
